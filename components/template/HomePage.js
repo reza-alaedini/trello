c4418c8e33e57547a5b8c5f4137df2c659ef83e0
Reza Alaedini <parsa.alaei.pa@gmail.com>
2023-09-13 16:14:25 +0330

    implement PATCH in front-end

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -11,26 +11,26 @@ function HomePage() {
   const fetchTodos = async () => {
     const res = await fetch("/api/todos");
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     if (data.status === "success") setTodos(data.todos);
   };
   return (
     <div className="home-page">
       <div className="home-page--todo">
         <p>Todo</p>
-        <Tasks data={todos.todo} />
+        <Tasks data={todos.todo} next="inProgress" fetchTodos={fetchTodos} />
       </div>
       <div className="home-page--inProgress">
         <p>In Progress</p>
-        <Tasks data={todos.inProgress} />
+        <Tasks data={todos.inProgress} next="review" back="todo" fetchTodos={fetchTodos} />
       </div>
       <div className="home-page--review">
         <p>Review</p>
-        <Tasks data={todos.review} />
+        <Tasks data={todos.review} next="done" back="inProgress" fetchTodos={fetchTodos} />
       </div>
       <div className="home-page--done">
         <p>Done</p>
-        <Tasks data={todos.done} />
+        <Tasks data={todos.done} back="review" fetchTodos={fetchTodos} />
       </div>
     </div>
   );
