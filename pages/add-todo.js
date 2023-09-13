cb555a7811bbe2d290e4fe84350380686530a0c6
Reza Alaedini <parsa.alaei.pa@gmail.com>
2023-09-13 18:16:37 +0330

    Server-side protection for add-todo & profile page

--- pages/add-todo.js
+++ pages/add-todo.js
@@ -1,7 +1,23 @@
 import AddTodoPage from "@/components/template/AddTodoPage";
+import { getSession } from "next-auth/react";
 
 function AddTodo() {
   return <AddTodoPage />;
 }
 
 export default AddTodo;
+
+// server-side protection
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+}
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,23 @@
 import ProfilePage from "@/components/template/ProfilePage";
+import { getSession } from "next-auth/react";
 
 function Profile() {
   return <ProfilePage />;
 }
 
 export default Profile;
+
+// server-side protection
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+}
