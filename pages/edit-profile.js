46ea2edcc77304b5a08f97b46a7e68c568bb8549
Reza Alaedini <parsa.alaei.pa@gmail.com>
2023-09-17 17:00:31 +0330

    server-side protection for edit-profile page

--- pages/edit-profile.js
+++ pages/edit-profile.js
@@ -1,7 +1,23 @@
 import EditProfilePage from "@/components/template/EditProfilePage";
+import { getSession } from "next-auth/react";
 
 function EditProfile() {
   return <EditProfilePage />;
 }
 
 export default EditProfile;
+
+// server-side protection
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+}
